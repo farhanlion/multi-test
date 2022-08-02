adb400a5b5afc255abce628af04bec79d0a8f95d
Neri007 <nerissagww@gmail.com>
2022-08-02 14:16:50 +0800

    update post route

--- routes/mview_display.js
+++ routes/mview_display.js
@@ -2,15 +2,18 @@ const { sequelize } = require("../db/models")
 const { QueryTypes } = require("sequelize")
 
 module.exports = function (app) {
-    
-    app.post("/mview_display", (req,res,next) => {
+    let bodyParser = require('body-parser')
+    let jsonParser = bodyParser.json()
+    let rawParser = bodyParser.raw()
+    let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+    app.post("/mview_display",urlencodedParser, (req,res,next) => {
         const db = require("../db/models")
         const Video = db.videos
         const Match = db.matches
         const Match_user = db.match_users
         const User = db.users
         let matchId = req.body.match_id
-        //console.log(matchId)
         if(!matchId){
             throw new Error("No such Match ID")
         }
@@ -63,7 +66,7 @@ module.exports = function (app) {
             })
 
             if(numOfVideos==5){
-                res.render("mview_display",{
+                res.render("pages/mview_display",{
                     //for html
                     tabTitle: "MULTI-VIEW",
                     numOfVideos: numOfVideos,
@@ -85,6 +88,9 @@ module.exports = function (app) {
                     stime4: sTimes[3],
                     stime5: sTimes[4],
                 })
+            }
+            else{
+                throw new Error("Incorrect Number of Videos")
             }
  
         }).catch((err)=>{
