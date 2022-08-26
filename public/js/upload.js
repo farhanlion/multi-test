fccb3fed671827b86df52a4d29d77864ba3822c0
farhan ahsan <bbss1e3farhan@gmail.com>
2022-08-26 16:34:28 +0800

    slider events

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,23 +3,50 @@
 player1 = cld.videoPlayer('player1',
         {
             loop: false,
-            controls: false,
+            controls: true,
             autoplay: false,
             q_auto: true
         });
 
+player1.source("https://res.cloudinary.com/dvapwslkg/video/upload/v1661432529/Vids/ttk1kqy6cjo0wkewv7jg.mp4");
+
 var slider = document.getElementById('slider');
 
 noUiSlider.create(slider, {
-  start: [20, 80],
+  start: [0, 80],
   connect: true,
   range: {
       'min': 0,
       'max': 100
   }
 });
+player1.on('loadeddata', (event) => {
+  duration = parseInt(player1.duration())
+  slider.noUiSlider.updateOptions({
+    range: {
+      'min': 0,
+      'max': duration
+    }
+  })
+})
 
+player1.on('timeupdate', (event) => {
+  var currentTime = parseInt(player1.currentTime())
+  console.log(currentTime)
+  slider.noUiSlider.setHandle(0, currentTime, false, false);
+})
+
+slider.noUiSlider.on('slide', function (values, handle) {
+  if (handle === 0) {
+    player1.currentTime(values[handle]);
+  }
+
+});
+
+
+//upload functionality
 $(document).ready(function() {
+
   $.cloudinary.config({ cloud_name: 'dvapwslkg', secure: true});
 
   //upload video
@@ -40,11 +67,14 @@ $(document).ready(function() {
       contentType: "application/json; charset=utf-8" // <- this is what you should add
     });
     console.log('connected to match')
-    // $('.preview').append(
-    //    $.cloudinary.videoTag(data.result.public_id, {id:'player1',  class:"cld-video-player",controls: false, secure: true, source_types:['mp4']}).toHtml()
-    //    );
-    player1.source("https://res.cloudinary.com/dvapwslkg/video/upload/v1661423766/"+data.result.public_id+"."+data.result.format);
-    player1.play();
+    // player1.source("https://res.cloudinary.com/dvapwslkg/video/upload/v1661423766/"+data.result.public_id+"."+data.result.format);
+    // player1.play();
+    //   slider.noUiSlider.updateOptions({
+    //     range: {
+    //         'min': 20,
+    //         'max': 30
+    //     }
+    // });
       });
 
     // upload progress
